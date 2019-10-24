54ebb9a3bcb5d08f6cf23f8a35a8d98ac8d71b30
D3stros <patrick.fallmann@hotmail.com>
2019-10-24 22:15:34 +0200

    Passing data with props

---
+++ src/components/AllFriends.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="all-friends">
+    <h2>All Friends</h2>
+    <div v-for="friend in friends" v-bind:key="friend.name">
+      <span>{{friend.name}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AllFriends',
+  props: ['friends'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
